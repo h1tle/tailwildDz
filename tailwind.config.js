e413a7a8fa44cb45956bfc3a3a6f0f9149ce67ff
Denys Shukhat <h1tle.cs@gmail.com>
2023-12-14 13:20:49 +0100

    first

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        main: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
